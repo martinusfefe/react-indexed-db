ba1d514487efd4bee0be00bb62fbd1cdca9aa7fc
martinusfefe <ja.martinus.ja@gmail.com>
2024-02-25 14:55:08 +0100

    window

---
+++ lib/indexed-context.d.ts
@@ -0,0 +1,42 @@
+import React, { ReactNode } from "react";
+import { Key } from "./indexed-db";
+interface IndexedDBProps {
+    name: string;
+    version: number;
+    children: ReactNode;
+    objectStoresMeta: ObjectStoreMeta[];
+}
+interface ObjectStoreMeta {
+    store: string;
+    storeConfig: {
+        keyPath: string;
+        autoIncrement: boolean;
+        [key: string]: any;
+    };
+    storeSchema: ObjectStoreSchema[];
+}
+interface ObjectStoreSchema {
+    name: string;
+    keypath: string;
+    options: {
+        unique: boolean;
+        [key: string]: any;
+    };
+}
+export declare function IndexedDB({ name, version, children, objectStoresMeta, }: IndexedDBProps): React.JSX.Element;
+interface AccessDBProps {
+    children: ({ db, }: {
+        db: IDBDatabase;
+        add: <T = any>(value: T, key?: any) => Promise<number>;
+        getByID: <T = any>(id: number | string) => Promise<T>;
+        getAll: <T = any>() => Promise<T[]>;
+        update: <T = any>(value: T, key?: any) => Promise<any>;
+        deleteRecord: (key: Key) => Promise<any>;
+        openCursor: (cursorCallback: (event: Event) => void, keyRange?: IDBKeyRange) => Promise<void>;
+        getByIndex: (indexName: string, key: any) => Promise<any>;
+        clear: () => Promise<any>;
+    }) => ReactNode;
+    objectStore: string;
+}
+export declare function AccessDB({ children, objectStore }: AccessDBProps): React.JSX.Element;
+export {};
